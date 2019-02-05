254131d047615a02d6436385250f0ff8e96f3f3b
vhernandezr <47341631+vhernandezr@users.noreply.github.com>
2019-02-05 10:27:28 -0500

    Add files via upload

---
+++ E-2/sketch.js
@@ -0,0 +1,412 @@
+var x=0;
+
+var x2=25;
+
+function setup() {
+  createCanvas(400, 400);
+  noStroke();
+  fill(30, 30, 15);
+  //tri derecho
+for( var v=0; v< 50;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+  
+//tri al revex
+  for(var i=50; i <100; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+
+    ///
+   
+ x=25;
+ x2=50;
+
+     //tri derecho
+for( var v=0; v< 25;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+  
+//tri revex
+  for(var i=25; i <75; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+    
+    //tri derecho
+for( var v=75; v< 100;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+    
+    ///
+    x=50;
+ x2=75;
+//tri revex
+  for(var i=0; i <50; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+     //tri derecho
+for( var v=50; v< 100;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+  ///
+    x=75;
+ x2=100;
+//tri revex
+  for(var i=0; i <25; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+     //tri derecho
+for( var v=25; v< 75;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+//tri revex
+  for(var i=75; i <100; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+////////////////////////////////////////////////
+
+x=100;
+ x2=125;
+
+//tri derecho
+for( var v=0; v< 25;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+  
+//tri al revex
+  for(var i=25; i <75; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+    
+    //tri derecho
+for( var v=75; v< 100;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+  
+  /////
+  x=125;
+ x2=150;
+  //tri derecho
+for( var v=0; v< 50;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+  
+//tri al revex
+  for(var i=50; i <100; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+    
+  ///
+  x=150;
+ x2=175;
+  //tri al revex
+  for(var i=0; i <25; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+     //tri derecho
+for( var v=25; v<75 ;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+   //tri al revex
+  for(var i=75; i <100; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+    ////
+     x=175;
+ x2=200;
+  //tri al revex
+  for(var i=0; i <50; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+     //tri derecho
+for( var v=50; v<100 ;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+  /////////////////////////////////////////////
+  
+  x=0;
+ x2=25;
+  //tri al revex
+  for(var i=100; i <150; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+     //tri derecho
+for( var v=150; v<200 ;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+  ///
+  
+  x=25;
+ x2=50;
+
+//tri derecho
+for( var v=100; v< 125;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+  
+//tri al revex
+  for(var i=125; i <175; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+    
+    //tri derecho
+for( var v=175; v< 200;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+  
+    /////
+    x=50;
+ x2=75;
+  //tri derecho
+for( var v=100; v< 150;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+  
+//tri al revex
+  for(var i=150; i <200; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+    ///
+     x=75;
+ x2=100;
+ //tri al revex
+  for(var i=100; i <125; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+     //tri derecho
+for( var v=125; v<175 ;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+   //tri al revex
+  for(var i=175; i <200; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+    /////
+    
+    
+    x=100;
+ x2=125;
+    fill(250, 188, 8);
+  
+     //tri derecho
+for( var v=100; v< 125;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+  
+//tri revex
+  for(var i=125; i <175; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+    
+    //tri derecho
+for( var v=175; v< 200;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+    
+    ////
+    
+       x=125;
+ x2=150;
+    //tri revex
+  for(var i=100; i <150; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+     //tri derecho
+for( var v=150; v< 200;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+    ///
+  x=150;
+ x2=175;
+//tri revex
+  for(var i=100; i <125; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+     //tri derecho
+for( var v=125; v< 175;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+//tri revex
+  for(var i=175; i <200; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+    ///
+    x=175;
+ x2=200;
+    //tri derecho
+for( var v=100; v< 150;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+  
+//tri al revex
+  for(var i=150; i <200; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+/////////////////////////////////////////////////////////////////////////////////////////
+
+  noStroke();
+  fill(30, 30, 15);
+  
+  x=200;
+ x2=225;
+  //tri derecho
+for( var v=0; v< 50;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+  
+//tri al revex
+  for(var i=50; i <100; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+
+    ///
+   
+ x=225;
+ x2=250;
+
+     //tri derecho
+for( var v=0; v< 25;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+  
+//tri revex
+  for(var i=25; i <75; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+    
+    //tri derecho
+for( var v=75; v< 100;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+    
+    ///
+    x=250;
+ x2=275;
+//tri revex
+  for(var i=0; i <50; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+     //tri derecho
+for( var v=50; v< 100;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+  ///
+    x=275;
+ x2=300;
+//tri revex
+  for(var i=0; i <25; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+     //tri derecho
+for( var v=25; v< 75;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+//tri revex
+  for(var i=75; i <100; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+////////////////////////////////////////////////
+
+x=300;
+ x2=325;
+
+//tri derecho
+for( var v=0; v< 25;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+  
+//tri al revex
+  for(var i=25; i <75; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+    
+    //tri derecho
+for( var v=75; v< 100;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+  
+  /////
+  x=325;
+ x2=350;
+  //tri derecho
+for( var v=0; v< 50;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+  
+//tri al revex
+  for(var i=50; i <100; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+    
+  ///
+  x=350;
+ x2=375;
+  //tri al revex
+  for(var i=0; i <25; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+     //tri derecho
+for( var v=25; v<75 ;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+   //tri al revex
+  for(var i=75; i <100; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+    ////
+     x=375;
+ x2=400;
+  //tri al revex
+  for(var i=0; i <50; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+     //tri derecho
+for( var v=50; v<100 ;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+ //// /////////////////////////////////////////////
+     fill(250, 188, 8);
+  x=200;
+ x2=225;
+  //tri al revex
+  for(var i=100; i <150; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+     //tri derecho
+for( var v=150; v<200 ;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+  ///
+  
+  x=225;
+ x2=250;
+
+//tri derecho
+for( var v=100; v< 125;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+  
+//tri al revex
+  for(var i=125; i <175; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+    
+    //tri derecho
+for( var v=175; v< 200;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+  
+    /////
+    x=250;
+ x2=275;
+  //tri derecho
+for( var v=100; v< 150;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+  
+//tri al revex
+  for(var i=150; i <200; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+    ///
+     x=275;
+ x2=300;
+
+ //tri al revex
+  for(var i=100; i <125; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+     //tri derecho
+for( var v=125; v<175 ;v= v+ 25){
+  triangle(x, v+25, x2, v , x2, v+25);}
+   //tri al revex
+  for(var i=175; i <200; i= i+ 25){
+    triangle( x, i+25, x2, i, x, i)}
+    /////
+    
+    fill(30, 30, 15);
+    x=300;
+ x2=325;
+    
+     //tri derecho
+for( var v=100; v< 125;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+  
+//tri revex
+  for(var i=125; i <175; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+    
+    //tri derecho
+for( var v=175; v< 200;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+    
+    ////
+    
+       x=325;
+ x2=350;
+    //tri revex
+  for(var i=100; i <150; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+     //tri derecho
+for( var v=150; v< 200;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+    ///
+  x=350;
+ x2=375;
+//tri revex
+  for(var i=100; i <125; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+     //tri derecho
+for( var v=125; v< 175;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+//tri revex
+  for(var i=175; i <200; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+    ///
+    x=375;
+ x2=400;
+    //tri derecho
+for( var v=100; v< 150;v= v+ 25){
+  triangle(x, v, x2, v + 25, x, v+25);}
+  
+//tri al revex
+  for(var i=150; i <200; i= i+ 25){
+    triangle( x, i, x2, i, x2, i+ 25)}
+
+//////////////////Por fin////////////////////////////////////////////////////////////
+ 
+}
+
+
+ 
